2a84a57ff29cd920be81aadd6a684aa66d9048d9
Fouad Chaker <96481150+FouadChaker@users.noreply.github.com>
2022-08-08 10:09:31 +0100

    add files via upload 'configservice'

---
+++ TpALConfigService/src/test/java/com/tp/TpAlConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAlConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
